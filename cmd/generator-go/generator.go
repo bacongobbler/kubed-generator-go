06feef598e5f00ae0592b45ffaee3e31a1eb40f1
Matthew Fisher <matt.fisher@microsoft.com>
2018-06-21 15:16:02 -0700

    fix indentation (again)

--- cmd/generator-go/generator.go
+++ cmd/generator-go/generator.go
@@ -37,17 +37,17 @@ CMD ["app"]
 	maingo = `package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World, I'm a Go app!\n")
+	fmt.Fprintf(w, "Hello World, I'm a Go app!\n")
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
 `
 	deploymentTemplate = `kind: Deployment
